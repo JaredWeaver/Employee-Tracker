5d6eaacc2704dfc86b17f72e3febe9633902604b
JaredWeav <jared.weaver2@gmail.com>
2021-02-22 15:13:38 -0500

    added files, mapped out functions

---
+++ prompts.js
@@ -0,0 +1,72 @@
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const figlet = require('figlet');
+const chalkAnimation = require('chalk-animation');
+const consoleTable = require('console.table');
+
+const mainMenu = () => {
+
+console.log('====================================================================================================')
+
+console.log(chalk.greenBright(figlet.textSync('Employee Tracker')))
+
+console.log(chalk.magentaBright('Created By: Jared Weaver'));
+
+
+console.log('====================================================================================================')
+//begins questioning 
+
+}
+
+const addDepartment = () => {
+
+}
+
+const addRoles = () => {
+
+}
+
+const addEmployees= () => {
+
+}
+
+const viewDepartments = () => {
+
+}
+
+const viewRoles = () => {
+
+}
+
+const viewEmployees = () => {
+
+}
+
+const updateRoles = () => {
+
+}
+
+//BONUS FUNCTIONS
+
+const updateEmployeeMgr = () => {
+
+}
+
+const viewEmployeeByMgr = () => {
+
+};
+
+const deleteObject = (department, role, manager) => {
+
+};
+
+const combineSalaries = () => {
+
+};
+
+
+
+
+
+
+mainMenu();
